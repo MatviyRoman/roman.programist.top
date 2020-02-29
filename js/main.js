a172886edf3b8019fe240093c820346059a5c2ad
Roman Matviy <roman@matviy.pp.ua>
2020-02-29 18:53:00 +0200

    add animation progress bar

--- js/main.js
+++ js/main.js
@@ -49,30 +49,84 @@ function greeting() {
 
 document.addEventListener("DOMContentLoaded", function(event) {
   // var elem = document.getElementById("bar");
-  // var width = 1;
   // var id = setInterval(frame, 100);
-
+  // var x = (document.getElementById("demo").innerHTML = x);
   // function frame() {
+  //   var width = document.getElementById("value").value;
   //   if (width >= 100) {
   //     clearInterval(id);
   //   } else {
+  //     var width = document.getElementById("value").value;
   //     width++;
   //     elem.style.width = width + "%";
-  //     document.getElementById("value").innerHTML = width + "%";
+  //     document.getElementById("number").innerHTML = width + "%";
   //   }
   // }
-
-  var elem = document.getElementById("bar");
-  var id = setInterval(frame, 100);
-  let width = document.getElementById("value").value;
-  var x = (document.getElementById("demo").innerHTML = x);
-  function frame() {
-    if (width >= 100) {
-      clearInterval(id);
-    } else {
-      width++;
-      elem.style.width = width + "%";
-      document.getElementById("value").innerHTML = width + "%";
-    }
-  }
 });
+
+function skillsetAnim() {
+  const p = document.querySelectorAll(".skillset progress");
+  console.log(p.length);
+
+  for (let i = 0; i <= p.length; i++) {
+    let val = Math.floor(p[i].getAttribute("number"));
+    p[i].setAttribute("value", val);
+    p[i].style.setProperty("--value", val + "%");
+  }
+}
+
+function languagesAnim() {
+  const p = document.querySelectorAll(".languages progress");
+  console.log(p.length);
+
+  for (let i = 0; i <= p.length; i++) {
+    let val = Math.floor(p[i].getAttribute("number"));
+    p[i].setAttribute("value", val);
+    p[i].style.setProperty("--value", val + "%");
+  }
+}
+
+window.onload = function() {
+  skillset.onmouseover = skillset.onmouseout = eventSkillset;
+  languages.onmouseover = languages.onmouseout = eventLanguages;
+  //test.onmouseover = test.onmouseout = handler;
+
+  function eventSkillset(event) {
+    function str(el) {
+      if (!el) return "null";
+      return el.className || el.tagName;
+    }
+    if (event.type == "mouseover") {
+      skillsetAnim();
+    }
+    // if (event.type == "mouseout") {
+    //   languagesAnim();
+    // }
+  }
+
+  function eventLanguages(event) {
+    function str(el) {
+      if (!el) return "null";
+      return el.className || el.tagName;
+    }
+    if (event.type == "mouseover") {
+      languagesAnim();
+    }
+    // if (event.type == "mouseout") {
+    //   languagesAnim();
+    // }
+  }
+
+  // function handler(event) {
+  //   function str(el) {
+  //     if (!el) return "null";
+  //     return el.className || el.tagName;
+  //   }
+  //   if (event.type == "mouseover") {
+  //     skillsetAnim();
+  //   }
+  //   // if (event.type == "mouseout") {
+  //   //   languagesAnim();
+  //   // }
+  // }
+};
