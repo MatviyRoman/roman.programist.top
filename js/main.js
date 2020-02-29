b45ba8773d9dda1290a9055c9dd8c45826f9b066
Roman Matviy <roman@matviy.pp.ua>
2020-02-29 13:02:10 +0200

    organization files in project

--- js/main.js
+++ js/main.js
@@ -13,39 +13,66 @@ function greeting() {
   document.getElementById("welcome").innerHTML = greeting;
 }
 
-$(".languages").hover(
-  function() {
-    let i = 0;
-    function move() {
-      if (i == 0) {
-        i = 0;
-        let elem = document.getElementById("myBar");
-        let number = document.getElementById("number");
-        var width = 0;
-        let id = setInterval(frame, 50);
-        function frame() {
-          if (width >= 80) {
-            clearInterval(id);
-            i = 0;
-          } else {
-            width++;
-            elem.style.width = width + "%";
-            number.innerHTML = width + "%";
-          }
-        }
-      }
-    }
-  }
-  // ,
-  // function() {
-  //   width++;
-  //   elem.style.width = width + "%";
-  //   number.innerHTML = width + "%";
-  //   $("#myBar").addClass("myBar");
-  // }
-  //, function () { $(this).removeClass('hover') }
-);
+//$(".languages")
+//.hover
+// function() {
+//   let i = 0;
+//   function move() {
+//     if (i == 0) {
+//       i = 0;
+//       let elem = document.getElementById("myBar");
+//       let number = document.getElementById("number");
+//       var width = 0;
+//       let id = setInterval(frame, 50);
+//       function frame() {
+//         if (width >= 80) {
+//           clearInterval(id);
+//           i = 0;
+//         } else {
+//           width++;
+//           elem.style.width = width + "%";
+//           number.innerHTML = width + "%";
+//         }
+//       }
+//     }
+//   }
+// }
+// ,
+// function() {
+//   width++;
+//   elem.style.width = width + "%";
+//   number.innerHTML = width + "%";
+//   $("#myBar").addClass("myBar");
+// }
+//, function () { $(this).removeClass('hover') }
+//();
 
 document.addEventListener("DOMContentLoaded", function(event) {
-  //do work
+  // var elem = document.getElementById("bar");
+  // var width = 1;
+  // var id = setInterval(frame, 100);
+
+  // function frame() {
+  //   if (width >= 100) {
+  //     clearInterval(id);
+  //   } else {
+  //     width++;
+  //     elem.style.width = width + "%";
+  //     document.getElementById("value").innerHTML = width + "%";
+  //   }
+  // }
+
+  var elem = document.getElementById("bar");
+  var id = setInterval(frame, 100);
+  let width = document.getElementById("value").value;
+  var x = (document.getElementById("demo").innerHTML = x);
+  function frame() {
+    if (width >= 100) {
+      clearInterval(id);
+    } else {
+      width++;
+      elem.style.width = width + "%";
+      document.getElementById("value").innerHTML = width + "%";
+    }
+  }
 });
