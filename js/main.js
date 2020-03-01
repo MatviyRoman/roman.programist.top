8ae834d0fbfab1c6c4fe1fb666cc546c6846b89f
Roman Matviy <roman@matviy.pp.ua>
2020-03-01 15:07:31 +0200

    Number sync with progress bar

--- js/main.js
+++ js/main.js
@@ -1,88 +1,49 @@
-new WOW().init();
+document.addEventListener('DOMContentLoaded', function(event) {
+  new WOW().init();
 
-function greeting() {
-  let time = new Date().getHours();
-  let greeting;
-  if (time < 10) {
-    greeting = "Good morning";
-  } else if (time < 20) {
-    greeting = "Good day";
-  } else {
-    greeting = "Good evening";
+  function greeting() {
+    let time = new Date().getHours();
+    let greeting;
+    if (time < 10) {
+      greeting = 'Good morning';
+    } else if (time < 20) {
+      greeting = 'Good day';
+    } else {
+      greeting = 'Good evening';
+    }
+    document.getElementById('welcome').innerHTML = greeting;
   }
-  document.getElementById("welcome").innerHTML = greeting;
-}
-
-//$(".languages")
-//.hover
-// function() {
-//   let i = 0;
-//   function move() {
-//     if (i == 0) {
-//       i = 0;
-//       let elem = document.getElementById("myBar");
-//       let number = document.getElementById("number");
-//       var width = 0;
-//       let id = setInterval(frame, 50);
-//       function frame() {
-//         if (width >= 80) {
-//           clearInterval(id);
-//           i = 0;
-//         } else {
-//           width++;
-//           elem.style.width = width + "%";
-//           number.innerHTML = width + "%";
-//         }
-//       }
-//     }
-//   }
-// }
-// ,
-// function() {
-//   width++;
-//   elem.style.width = width + "%";
-//   number.innerHTML = width + "%";
-//   $("#myBar").addClass("myBar");
-// }
-//, function () { $(this).removeClass('hover') }
-//();
-
-document.addEventListener("DOMContentLoaded", function(event) {
-  // var elem = document.getElementById("bar");
-  // var id = setInterval(frame, 100);
-  // var x = (document.getElementById("demo").innerHTML = x);
-  // function frame() {
-  //   var width = document.getElementById("value").value;
-  //   if (width >= 100) {
-  //     clearInterval(id);
-  //   } else {
-  //     var width = document.getElementById("value").value;
-  //     width++;
-  //     elem.style.width = width + "%";
-  //     document.getElementById("number").innerHTML = width + "%";
-  //   }
-  // }
 });
 
 function skillsetAnim() {
-  const p = document.querySelectorAll(".skillset progress");
-  console.log(p.length);
+  const p = document.querySelectorAll('.skillset progress');
+  const lp = document.querySelectorAll('.skillset li p'); //anim num
+  console.log(p.length, lp.length);
 
   for (let i = 0; i <= p.length; i++) {
-    let val = Math.floor(p[i].getAttribute("number"));
-    p[i].setAttribute("value", val);
-    p[i].style.setProperty("--value", val + "%");
+    let val = Math.floor(p[i].getAttribute('data-value'));
+    lp[i].setAttribute('style', 'width:' + val + '%'); //anim num
+    p[i].setAttribute('value', val);
+    p[i].style.setProperty('--value', val + '%');
   }
 }
 
 function languagesAnim() {
-  const p = document.querySelectorAll(".languages progress");
-  console.log(p.length);
+  const p = document.querySelectorAll('.languages progress');
+  const lp = document.querySelectorAll('.languages li p'); //anim num
+  console.log(p.length, lp.length);
 
   for (let i = 0; i <= p.length; i++) {
-    let val = Math.floor(p[i].getAttribute("number"));
-    p[i].setAttribute("value", val);
-    p[i].style.setProperty("--value", val + "%");
+    let val = Math.floor(lp[i].getAttribute('data-value'));
+    // for (let j = 0; j <= val; j++) {
+    //   // val += j;
+    //   lp[i].setAttribute('style', 'width:' + j + '%'); //anim num
+    //   lp[i].setAttribute('data-value', j); //anim num
+    //   p[i].setAttribute('value', j);
+    // } /// document.get style [data-value]
+    lp[i].setAttribute('style', 'width:' + val + '%'); //anim num
+    p[i].setAttribute('value', val);
+    p[i].style.setProperty('--value', val + '%');
   }
 }
 
@@ -93,28 +54,22 @@ window.onload = function() {
 
   function eventSkillset(event) {
     function str(el) {
-      if (!el) return "null";
+      if (!el) return 'null';
       return el.className || el.tagName;
     }
-    if (event.type == "mouseover") {
+    if (event.type == 'mouseover') {
       skillsetAnim();
     }
-    // if (event.type == "mouseout") {
-    //   languagesAnim();
-    // }
   }
 
   function eventLanguages(event) {
     function str(el) {
-      if (!el) return "null";
+      if (!el) return 'null';
       return el.className || el.tagName;
     }
-    if (event.type == "mouseover") {
+    if (event.type == 'mouseover') {
       languagesAnim();
     }
-    // if (event.type == "mouseout") {
-    //   languagesAnim();
-    // }
   }
 
   // function handler(event) {
